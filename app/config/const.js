3e39a29921f67f28bd617a1b090c346fe5679943
Dogson <gwenael.girod@gmail.com>
2018-12-05 16:02:13 +0000

    (feat)Added a bunch of place types

--- app/config/const.js
+++ app/config/const.js
@@ -1,8 +1,83 @@
 export const PLACE_TYPES = {
-    RESTAURANT: "Restaurant",
-    BAR: "Bar",
-    CINEMA: "Cinéma",
-    HOTEL: "Hotel"
+    RESTAURANT: {
+        id: 'restaurant',
+        name: "Restaurant",
+        icon: {
+            name: 'restaurant'
+        }
+    },
+    BAR: {
+        id: 'bar',
+        name: 'Bar',
+        icon: {
+            name: 'local-bar'
+        }
+    },
+    CINEMA: {
+        id: 'movie_theater',
+        name: 'Cinéma',
+        icon: {
+            name: 'local-movies'
+        }
+    },
+    HOTEL: {
+        id: 'lodging',
+        name: 'Hotel',
+        icon: {
+            name: 'hotel'
+        },
+    },
+    BOWLING: {
+        id: 'bowling_alley',
+        name: 'Bowling',
+        icon: {
+            name: 'bowling',
+            type: 'MaterialCommunityIcons'
+        },
+    },
+    CAFE: {
+        id: 'cafe',
+        name: 'Café',
+        icon: {
+            name: 'local-cafe'
+        },
+    },
+    CASINO: {
+        id: 'casino',
+        name: 'Casino',
+        icon: {
+            name: 'casino'
+        },
+    },
+    GYM: {
+        id: 'gym',
+        name: 'Salle de sport',
+        icon: {
+            name: 'fitness-center'
+        }
+    },
+    LIBRARY: {
+        id: 'library',
+        name: 'Bibliothèque',
+        icon: {
+            name: 'local-library'
+        }
+    },
+    MUSEUM: {
+        id: 'museum',
+        name: 'Musée',
+        icon: {
+            name: 'local-library'
+        }
+    },
+    CLUB: {
+        id: 'night_club',
+        name: 'Boîte de nuit',
+        icon: {
+            name: 'compact-disc',
+            type: 'FontAwesome5'
+        }
+    }
 };
 
 export const RATINGS = [
